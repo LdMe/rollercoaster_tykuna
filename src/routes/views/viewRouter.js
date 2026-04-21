a58562fd5a734b375ad3d9db5fc4425c20e7a704
Danel Lafuente <dlafuente003@gmail.com>
2026-04-21 19:45:14 +0200

    vista inicial

--- src/routes/views/viewRouter.js
+++ src/routes/views/viewRouter.js
@@ -2,7 +2,9 @@ import {Router} from "express";
 import rideViewRouter from "./rideViewRouter.js";
 import authRouter from "./authViewRouter.js";
 const router = Router();
-
+router.get("/",(req,res)=>{
+    res.render("index");
+})
 router.use("/rides",rideViewRouter);
 router.use("/auth",authRouter);
 
